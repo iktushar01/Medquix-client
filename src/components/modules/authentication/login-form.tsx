5bccad3e9ae2746c3ef907982004ebc9d4629f93
iktushar01 <iktushar2020@gmail.com>
2026-02-13 16:55:49 +0600

    remove fp

--- src/components/modules/authentication/login-form.tsx
+++ src/components/modules/authentication/login-form.tsx
@@ -6,7 +6,6 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
   Field,
-  FieldDescription,
   FieldGroup,
   FieldLabel,
   FieldSeparator,
@@ -14,8 +13,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { authClient } from "@/lib/auth-client";
 import { handleGoogleLogin } from "@/lib/handleGoogleLogin";
-import { Eye, EyeOff, GalleryVerticalEnd } from "lucide-react";
-import Link from "next/link"; // Using Next.js Link for faster navigation
+import { Eye, EyeOff } from "lucide-react";
+import Link from "next/link";
 
 export function LoginForm({
   className,
@@ -110,12 +109,7 @@ export function LoginForm({
           </Field>
 
           <Field>
-            <div className="flex items-center">
-              <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Link href="/forgot-password" className="ml-auto text-sm underline-offset-4 hover:underline">
-                Forgot?
-              </Link>
-            </div>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
             <div className="relative">
               <Input
                 id="password"
@@ -144,7 +138,9 @@ export function LoginForm({
           <FieldSeparator>Or continue with</FieldSeparator>
 
           <Button onClick={handleGoogleLogin} variant="outline" type="button" className="w-full">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="mr-2 h-4 w-4"><path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" fill="currentColor" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="mr-2 h-4 w-4">
+              <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" fill="currentColor" />
+            </svg>
             Google
           </Button>
         </FieldGroup>
