ec795b39197a7e29047a37f70d14cc0cef55a76b
iktushar01 <iktushar2020@gmail.com>
2026-02-13 17:09:44 +0600

    createAuthClient fix

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,8 @@
 import { createAuthClient } from "better-auth/react"
+
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:5000"
+  baseURL: 
+    process.env.NODE_ENV === "production"
+      ? "https://medquix-server.vercel.app"  // deployed backend
+      : "http://localhost:5000"              // local dev
 })
